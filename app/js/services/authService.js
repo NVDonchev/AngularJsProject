190c58b810e78b21eaa06471810555cf726dc1d5
NVDonchev <koko_q@abv.bg>
2015-05-30 17:43:58 +0300

    AuthService added.

---
+++ app/js/services/authService.js
@@ -0,0 +1,63 @@
+﻿'use strict';
+
+app.factory('authService',
+    function ($http, baseServiceUrl) {
+        return {
+            login: function (userData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/users/login',
+                    data: userData
+                };
+                $http(request).success(function (data) {
+                    sessionStorage['currentUser'] = JSON.stringify(data);
+                    success(data);
+                }).error(error);
+            },
+
+            register: function (userData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/users/register',
+                    data: userData
+                };
+                $http(request).success(function (data) {
+                    sessionStorage['currentUser'] = JSON.stringify(data);
+                    success(data);
+                }).error(error);
+            },
+
+            logout: function (success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/users/logout',
+                    headers: this.getAuthHeaders()
+                };
+                $http(request).success(function (data) {
+                    delete sessionStorage['currentUser'];
+                    success(data);
+                }).error(error);
+            },
+
+            getCurrentUser: function () {
+                var userObject = sessionStorage['currentUser'];
+                if (userObject) {
+                    return JSON.parse(sessionStorage['currentUser']);
+                }
+            },
+
+            isLoggedIn: function () {
+                return sessionStorage['currentUser'] != undefined;
+            },
+
+            getAuthHeaders: function () {
+                var headers = {};
+                var currentUser = this.getCurrentUser();
+                if (currentUser) {
+                    headers['Authorization'] = 'Bearer ' + currentUser.access_token;
+                }
+                return headers;
+            },
+        }
+    }
+);
