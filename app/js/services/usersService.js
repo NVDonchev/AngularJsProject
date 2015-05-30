9789fd4f473a941f4bdb65a07ea3a1e349508175
NVDonchev <koko_q@abv.bg>
2015-05-30 18:05:19 +0300

    UsersService finished.

---
+++ app/js/services/usersService.js
@@ -0,0 +1,117 @@
+﻿'use strict';
+
+app.factory('authService',
+    function ($http, baseServiceUrl) {
+        return {
+            login: function (userData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/users/login',
+                    data: userData
+                };
+                $http(request).success(function (data) {
+                    sessionStorage['currentUser'] = JSON.stringify(data);
+                    success(data);
+                }).error(error);
+            },
+
+            register: function (userData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/users/register',
+                    data: userData
+                };
+                $http(request).success(function (data) {
+                    sessionStorage['currentUser'] = JSON.stringify(data);
+                    success(data);
+                }).error(error);
+            },
+
+            logout: function (success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/users/logout',
+                    headers: this.getAuthHeaders()
+                };
+                $http(request).success(function (data) {
+                    delete sessionStorage['currentUser'];
+                    success(data);
+                }).error(error);
+            },
+
+            getCurrentUser: function () {
+                var userObject = sessionStorage['currentUser'];
+                if (userObject) {
+                    return JSON.parse(sessionStorage['currentUser']);
+                }
+            },
+
+            isLoggedIn: function () {
+                return sessionStorage['currentUser'] != undefined;
+            },
+
+            getAuthHeaders: function () {
+                var headers = {};
+                var currentUser = this.getCurrentUser();
+                if (currentUser) {
+                    headers['Authorization'] = 'Bearer ' + currentUser.access_token;
+                }
+                return headers;
+            },
+
+            getUserPreviewData: function (user, success, error) {
+                var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/users/' + user + '/preview',
+                    headers: authService.getAuthHeaders(),
+                };
+                $http(request).success(success).error(error);
+            },
+
+            getUserFullData: function (user, success, error) {
+                var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/api/users/' + user,
+                    headers: authService.getAuthHeaders(),
+                };
+                $http(request).success(success).error(error);
+            },
+
+            searchUserByName: function (searchTerm, success, error) {
+                var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/api/users/search?searchTerm=' + searchTerm,
+                    headers: authService.getAuthHeaders(),
+                };
+                $http(request).success(success).error(error);
+            },
+
+            getFriendWallByPages: function (user, success, error) { // TODO: Make pagination work
+                var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/api/users/' + user + '/wall?StartPostId=&PageSize=5',
+                    headers: authService.getAuthHeaders(),
+                };
+                $http(request).success(success).error(error);
+            },
+
+            getFriendDetailedFriends: function (user, success, error) {
+                var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/api/users/' + user + '/friends',
+                    headers: authService.getAuthHeaders(),
+                };
+                $http(request).success(success).error(error);
+            },
+
+            getFriendFriendsPreview: function (user, success, error) {
+                var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/users/' + user + '/friends/preview',
+                    headers: authService.getAuthHeaders(),
+                };
+                $http(request).success(success).error(error);
+            },
+        }
+    }
+);
