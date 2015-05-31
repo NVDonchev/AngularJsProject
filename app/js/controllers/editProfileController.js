b65807229424cace2f83da98c29506437244c55f
NVDonchev <koko_q@abv.bg>
2015-05-31 16:42:20 +0300

    Edit page added. Self-evaluation file added.

---
+++ app/js/controllers/editProfileController.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+app.controller('EditProfileController',
+    function ($scope, $location, profileService, notifyService) {
+        $scope.submitChanges = function (editData) {
+            profileService.editCurrentUserData(
+                editData,
+                function success() {
+                    notifyService.showInfo("Edit profile successful");
+                    $location.path("/home/");
+                },
+                function error(err) {
+                    notifyService.showError("Edit profile failed", err);
+                }
+            );
+        };
+    }
+);
